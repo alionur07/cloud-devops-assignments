e4233277a74d7dcaca601e3783d4103bb93bd0fd
AOA <alionur.aktepe@gmail.com>
2022-01-24 18:34:53 +0300

    deserilaze issue

--- src/main/java/com/contoso/payroll/Employee.java
+++ src/main/java/com/contoso/payroll/Employee.java
@@ -1,7 +1,7 @@
 package com.contoso.payroll;
 import javax.persistence.Table;
 import java.util.Objects;
-
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
