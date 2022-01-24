eaa8f4cae2f8f75d14e01fb40352b9f461f85073
AOA <alionur.aktepe@gmail.com>
2022-01-24 18:35:54 +0300

    deserilaze issue

--- src/main/java/com/contoso/payroll/Manager.java
+++ src/main/java/com/contoso/payroll/Manager.java
@@ -6,7 +6,7 @@ import javax.persistence.Table;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+import java.io.Serializable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
